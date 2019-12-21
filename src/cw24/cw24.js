efea676d28cee9984aa09bdb664dcc27c799b042
Grzegorz <sa@op.pl>
2019-12-21 12:34:02 +0100

    wiczenia i przykłady do przerwy II

---
+++ src/cw24/cw24.js
@@ -0,0 +1,3 @@
+function testScreenObjects() {
+    document.getElementById("demo").innerHTML = "Screen hight is: " + screen.availHeight + " screen width is: " + screen.availWidth + + " Color depht is: " + screen.colorDepth + " Screen orientation is: " + screen.orientation
+}
